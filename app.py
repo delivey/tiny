b2a71e3a99f90d679c9ee20715a67a18507d7b2c
delivey <deliveydev@gmail.com>
2021-10-27 16:31:42 +0300

    MVP complete

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect, request, jsonify
 import psycopg2
 from dotenv import load_dotenv
 import os
@@ -8,20 +8,12 @@ load_dotenv()
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template("index.html")
-
-@app.route("/api/original")
-def original():
-    return redirect("/")
-
+# Returns an url code.
 def generate_url():
-    length = 7
-    return "u" + ''.join([choice(ascii_uppercase + ascii_lowercase) for char in range(length)])
+    length = 6
+    return "u" + ''.join([choice(ascii_uppercase + ascii_lowercase) for _ in range(length)])
 
-@app.route("/u<code>", methods=["GET"])
-def get(code):
+def get_url_from_code(code):
     conn = psycopg2.connect(
         host=os.getenv("POSTGRES_HOST"),
         database=os.getenv("POSTGRES_DATABASE"),
@@ -32,15 +24,32 @@ def get(code):
 
     db.execute("SELECT original FROM urls WHERE new=%s", ("u"+code,))
     original = db.fetchone()[0]
-    return redirect(original)
+    conn.close()
+    return original
+
+# Routes for demo
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route("/u<code>", methods=["GET"])
+def get(code):
+    url = get_url_from_code(code)
+    return redirect(url)
+
+# Routes for API
+@app.route("/api/original")
+def original():
+    code = request.json.code
+    url = get_url_from_code(code)
+    return jsonify({"url": url})
 
 @app.route("/api/shorten", methods=["POST"])
 def shorten():
 
-    # Supports both JSON and urlencoded.
+    # Supports both JSON and urlencoded (required for demo)
     data = request.json
-    if data == None:
-        data = request.form
+    if data == None: data = request.form
 
     conn = psycopg2.connect(
         host=os.getenv("POSTGRES_HOST"),
